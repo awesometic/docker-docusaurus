72deb4d235d062dee51b0587928b31216a6cbd46
Deokgyu Yang <secugyu@gmail.com>
2021-08-20 12:40:12 +0900

    Improve the update script
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- config/auto_update_job.sh
+++ config/auto_update_job.sh
@@ -6,13 +6,17 @@ msg() {
 
 WEB_SRC_PATH="/docusaurus/website"
 
-cd "$WEB_SRC_PATH" || msg "Cannot enter $WEB_SRC_PATH. Won't run the yarn command to update."; exit
+cd "$WEB_SRC_PATH" || (msg "Cannot enter $WEB_SRC_PATH. Won't run the yarn command to update." ; exit)
 
-msg "Current version: $(yarn docusaurus --version)"
+msg "Current version"
+yarn docusaurus --version
+
 msg "Now updating to the latest version..."
 yarn install &
 [[ "$!" -gt 0 ]]; wait "$!"
-msg "Updated version: $(yarn docusaurus --version)"
+
+msg "Updated version"
+yarn docusaurus --version
 
 exit 0
 
